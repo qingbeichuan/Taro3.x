9d1137029b79afb2a8e1dc37735b75e431b367d7
qingbeichuan <qingbeichuan@163.com>
2021-06-11 00:31:31 +0800

    add tools

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,11 +1,68 @@
+/**
+ * @description: 本地存储
+ * @param {string}
+ */
 export const storage = {
   set(key, val) {
-    wx.setStorageSync(key, val);
+    Taro.setStorageSync(key, val);
   },
   get(key) {
-    return wx.getStorageSync(key);
+    return Taro.getStorageSync(key);
   },
   remove(key) {
-    wx.removeStorageSync(key);
+    Taro.removeStorageSync(key);
   }
 }
+
+/**
+ * @description: 防抖
+ * @param {function} fn
+ * @param {number} timestamp
+ * @return {function}
+ */
+export function debounce(fn, interval = 400) {
+  let timer = null
+  return function () {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+    }, interval)
+  }
+}
+
+/**
+ * @description: 节流
+ * @param {function} fn
+ * @param {number} timestamp
+ * @return {function}
+ */
+export function throttle(fn, interval = 300) {
+  let timer = null
+  return function () {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, arguments)
+        timer = null
+      }, interval)
+    }
+  }
+}
+
+/**
+ * @description: 解析url参数
+ * @param {string} url
+ * @return {object}
+ */
+export function urlParse(url) {
+  const [obj, reg] = [{}, /[?&][^?&]+=[^?&]+/g]
+  const arr = url.search.match(reg);  // ['?id=12345', '&a=b']
+  if (arr) {
+    arr.forEach((item) => {
+      let tempArr = item.substring(1).split('=');
+      let key = decodeURIComponent(tempArr[0]);
+      let val = decodeURIComponent(tempArr[1]);
+      obj[key] = val;
+    });
+  }
+  return obj;
+};
